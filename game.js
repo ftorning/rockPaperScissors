0442e3bbcc99e7208a108881c07ab773d25cad98
Torning, Fraser <Fraser_Torning@comcast.com>
2018-05-17 21:09:31 -0400

    added score js variables

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 var gameSelection = document.getElementById("gametype");
 var scoreBoard = document.getElementById("scoreboard");
 var gameChoices = document.getElementById("choices");
+var player1scoredisplay = document.getElementById("player1score");
+var player2scoredisplay = document.getElementById("player2score");
 
-scoreBoard.style.visibility = "hidden";
-gameChoices.style.visibility = "hidden";
+var choice = {0:'ROCK', 1:'PAPER', 2:'SCISSORS'};
+var player1score = 0;
+var player2score = 0;
+
+// scoreBoard.style.visibility = "hidden";
+// gameChoices.style.visibility = "hidden";
+player1scoredisplay.textContent = player1score;
+player2scoredisplay.textContent = player2score;
