e32fe98071af9a8b5d3497ded44a3ed4f91765eb
Torning, Fraser <Fraser_Torning@comcast.com>
2018-05-17 20:56:51 -0400

    initial commit

---
+++ game.js
@@ -0,0 +1,6 @@
+var gameSelection = document.getElementById("gametype");
+var scoreBoard = document.getElementById("scoreboard");
+var gameChoices = document.getElementById("choices");
+
+scoreBoard.style.visibility = "hidden";
+gameChoices.style.visibility = "hidden";
